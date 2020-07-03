68681938b7948a6cf482a06ce380c87c48cd4092
Tantatorn-dev <timefear007@gmail.com>
2020-07-03 22:03:24 +0700

    add corr function

--- air_pollution.R
+++ air_pollution.R
@@ -1,5 +1,6 @@
 # first programming assignment
 
+# part 1
 pollutantmean <- function(directory, pollutant, id=1:332) {
   
   sum <- 0
@@ -24,6 +25,7 @@ pollutantmean <- function(directory, pollutant, id=1:332) {
   
 }
 
+# part 2
 complete <- function(directory, id=1:332) {
   
   # empty nobs vector
@@ -49,6 +51,39 @@ complete <- function(directory, id=1:332) {
   
 }
 
+# part 3
+corr <- function(directory, threshold = 0) {
+  
+  id <- 1:332
+  corr_vec <- c()
+  
+  # loop through all ids
+  for (i in id) {
+    
+    if ( complete(directory,i)['nobs'] > threshold ) {
+      # create full path to csv 
+      filepath <- sprintf("%s%03d.csv",directory,i)
+      
+      # read that csv
+      df <- read.csv(filepath)
+      df <- na.omit(df)
+      sulfate <- df['sulfate']
+      nitrate <- df['nitrate']
+      
+      # compute correlation
+      corr <- cor(sulfate,nitrate)
+      corr_vec <- c(corr_vec,corr)
+      
+    }
+    
+  }
+  
+  corr_vec
+  
+}
+
 pollutantmean("./datasets/specdata/","nitrate",23) 
 complete("./datasets/specdata/",30:25) 
-
+cr <- corr("./datasets/specdata/", 400)
+head(cr)
+summary(cr)
