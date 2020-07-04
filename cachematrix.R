8185075b74ad8fbfbc4711874f58d74f16035c5c
Tantatorn-dev <timefear007@gmail.com>
2020-07-05 00:05:47 +0700

    add cachematrix assignment 2

---
+++ cachematrix.R
@@ -0,0 +1,31 @@
+# Programming Assignment 2 
+# create a cache matrix
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+# find an inverse of our matrix
+cacheSolve <- function(x, ...) {
+      
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
